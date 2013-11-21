1d7fda1f540f53d5de92a09a9e2522b728bdfc5e
Bernhard Miklautz <bmiklautz@thinstuff.at>
2013-11-21 15:32:29 +0100

    xorg-build: fixed and updated debian builds
    also try optimized build on ubuntu. To disable use -DUBUNTU_DETECTION=ON

--- session-manager/module/X11/service/xorg-build/scripts/LocalConfigSettings.cmake
+++ session-manager/module/X11/service/xorg-build/scripts/LocalConfigSettings.cmake
@@ -1,2 +1,5 @@
 set(XOBJBASE_RELATIVE "../xorg-server/build-main")
+# ubuntu <= 13.10, debian wheezy
 set(ADDITIONAL_REQUIRED_LIBS "-lselinux -laudit -lgcrypt")
+# ubuntu >= 13.10
+#set(ADDITIONAL_REQUIRED_LIBS "-lselinux -laudit -lgcrypt -lglapi")
