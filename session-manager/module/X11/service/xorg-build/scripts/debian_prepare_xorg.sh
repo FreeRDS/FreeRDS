28806ac9c1ed9610478f9277cd077c9439eba16c
Mike McDonald <Mike.McDonald@software.dell.com>
2013-12-04 16:42:17 -0500

    Modified script to detect the correct version of xserver-org installed on the system

--- session-manager/module/X11/service/xorg-build/scripts/debian_prepare_xorg.sh
+++ session-manager/module/X11/service/xorg-build/scripts/debian_prepare_xorg.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-VERSION=`dpkg-query -W -f='${Version}\n' xserver-xorg-core | cut -d: -f 2 | cut -d'-' -f 1`
+#
+# Determine the actual package name and version for xserver-xorg-core
+#
+PACKAGE=`dpkg-query -W | awk '/xserver-xorg-core/ { print $1 }'`
+VERSION=`dpkg-query -W | awk '/xserver-xorg-core/ { print $2 }' | cut -d: -f 2 | cut -d'-' -f 1`
+SUFFIX=`echo ${PACKAGE} | sed 's/xserver-xorg-core//'`
+
+echo "package=${PACKAGE}"
+echo "version=${VERSION}"
+echo "suffix=${SUFFIX}"
 
 mkdir -p external/Source
 cd external/Source
 
-sudo apt-get build-dep xorg-server xserver-xorg-core
-apt-get source xserver-xorg-core
-ln -s xorg-server-${VERSION} xorg-server
+#sudo apt-get build-dep xorg-server${SUFFIX} ${PACKAGE}
+apt-get source xorg-server${SUFFIX}
+ln -s xorg-server${SUFFIX}-${VERSION} xorg-server
+
+cd ../..
