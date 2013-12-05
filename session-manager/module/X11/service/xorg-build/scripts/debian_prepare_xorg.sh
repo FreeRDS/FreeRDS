29e945455e0cc5a12237eea8869985d52cd1b4ec
Mike McDonald <Mike.McDonald@software.dell.com>
2013-12-05 08:29:58 -0500

    Added comments and did some cleanup

--- session-manager/module/X11/service/xorg-build/scripts/debian_prepare_xorg.sh
+++ session-manager/module/X11/service/xorg-build/scripts/debian_prepare_xorg.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #
-# Determine the actual package name and version for xserver-xorg-core
+# Grab the actual package name and version for xserver-xorg-core.  Note
+# that this name can differ on various Linux distros (e.g., the package
+# is xserver-xorg-core-lts-raring on Ubuntu 12.04 LTS).
 #
 PACKAGE=`dpkg-query -W | awk '/xserver-xorg-core/ { print $1 }'`
 VERSION=`dpkg-query -W | awk '/xserver-xorg-core/ { print $2 }' | cut -d: -f 2 | cut -d'-' -f 1`
@@ -14,17 +16,32 @@ echo "suffix=${SUFFIX}"
 mkdir -p external/Source
 cd external/Source
 
+#
+# This controls whether we use apt-get or grab sources directly
+# from xorg.freedesktop.org.
+#
 USE_APT_GET=1
 
 if [ ${USE_APT_GET} == 1 ]; then
-sudo apt-get build-dep xorg-server${SUFFIX} ${PACKAGE}
+
+#
+# Use apt-get to grab sources
+#
+#sudo apt-get build-dep xorg-server${SUFFIX} ${PACKAGE}
+sudo apt-get build-dep xorg-server${SUFFIX}
 apt-get source xorg-server${SUFFIX}
 ln -s xorg-server${SUFFIX}-${VERSION} xorg-server
+
 else
+
+#
+# Grab sources directly from xorg.freedesktop.org
+#
 XORG_NAME=xorg-server-${VERSION}
 
 wget http://xorg.freedesktop.org/releases/individual/xserver/${XORG_NAME}.tar.gz
 tar zxvf ${XORG_NAME}.tar.gz
 ln -s ${XORG_NAME} xorg-server
 rm ${XORG_NAME}.tar.gz
+
 fi
