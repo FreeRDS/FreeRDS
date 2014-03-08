4c2fb2f270d76aa39a37bc24c3eb79a6d642238a
Mike McDonald <Mike.McDonald@software.dell.com>
2014-03-08 08:12:09 -0500

    Added dependency for libgl1-mesa-dev

--- build-freerds.sh
+++ build-freerds.sh
@@ -89,7 +89,7 @@ sudo apt-get install -y \
 libpciaccess-dev libpam0g-dev libpng12-dev libjpeg-dev intltool \
 libexpat1-dev libxml-libxml-perl libtool bison flex xsltproc \
 libfreetype6-dev libfontconfig1-dev libpixman-1-dev xutils-dev \
-x11proto-gl-dev mesa-common-dev xorg-dev \
+x11proto-gl-dev mesa-common-dev libgl1-mesa-dev xorg-dev \
 protobuf-c-compiler libprotobuf-c0 libprotobuf-dev libprotobuf-c0-dev \
 libboost-dev
 
