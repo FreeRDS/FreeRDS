97bf639a012cd02f6eed3676b0fc84eab7703a79
Mike McDonald <Mike.McDonald@software.dell.com>
2014-03-26 10:49:46 -0400

    Updated build script to include proper dependencies for CentOS distributions

--- build-freerds.sh
+++ build-freerds.sh
@@ -32,9 +32,34 @@ FREERDS_BRANCH=awakecoding
 FREERDS_INSTALL_DIR=/opt/FreeRDS
 
 #
+# Determine the Linux distro
+#
+LINUX_DISTRO_NAME=`cat /etc/issue|head -1|awk '{ print $1 }'`
+
+case $LINUX_DISTRO_NAME in
+  Ubuntu)
+    LINUX_DISTRO_VERSION=`cat /etc/issue|head -1|awk '{ print $2 }'`
+    ;;		
+  CentOS)
+    LINUX_DISTRO_VERSION=`cat /etc/issue|head -1|awk '{ print $3 }'`
+    ;;
+  *)
+    echo "Unsupported Linux distro '$LINUX_DISTRO_NAME'"
+    exit
+    ;;
+esac
+
+#
 # Fetch sources from GitHub
 #
-sudo apt-get install -y git-core
+case $LINUX_DISTRO_NAME in
+  Ubuntu)
+    sudo apt-get install -y git-core
+    ;;
+  CentOS)
+    sudo yum install -y git-core
+    ;;
+esac
 
 if [ ! -d $GIT_ROOT_DIR ]; then
   mkdir -p $GIT_ROOT_DIR
@@ -59,7 +84,14 @@ fi
 #
 # Install the correct version of CMake
 #
-sudo apt-get install -y build-essential
+case $LINUX_DISTRO_NAME in
+  Ubuntu)
+    sudo apt-get install -y build-essential
+    ;;
+  CentOS)
+    sudo yum groupinstall -y "Development Tools"
+    ;;
+esac
 
 CMAKE_VERSION=2.8.12
 CMAKE_FOLDER=cmake-$CMAKE_VERSION
@@ -67,7 +99,7 @@ CMAKE_ARCHIVE=$CMAKE_FOLDER.tar.gz
 CMAKE_URL=http://www.cmake.org/files/v2.8/$CMAKE_ARCHIVE
 
 RESULT=`cmake --version`
-if [ "$RESULT" != "cmake version $CMAKE_VERSION" ]; then
+if [ "$RESULT" == "" ]; then
   # Download compressed archive
   pushd ~/Downloads
   wget $CMAKE_URL
@@ -83,40 +115,67 @@ if [ "$RESULT" != "cmake version $CMAKE_VERSION" ]; then
   popd
   rm $CMAKE_ARCHIVE
   popd
+else
+  echo Found $RESULT
 fi
 
 #
 # Install FreeRDP dependencies
 #
-sudo apt-get install -y \
-libssl-dev \
-libx11-dev libxext-dev libxinerama-dev libxcursor-dev libxkbfile-dev \
-libxv-dev libxi-dev libxdamage-dev libxrender-dev libxrandr-dev \
-libasound2-dev libcups2-dev libpulse-dev \
-libavutil-dev libavcodec-dev \
-libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+case $LINUX_DISTRO_NAME in
+  Ubuntu)
+    sudo apt-get install -y \
+    libssl-dev \
+    libx11-dev libxext-dev libxinerama-dev libxcursor-dev libxkbfile-dev \
+    libxv-dev libxi-dev libxdamage-dev libxrender-dev libxrandr-dev \
+    libasound2-dev libcups2-dev libpulse-dev \
+    libavutil-dev libavcodec-dev \
+    libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+	;;
+
+  CentOS)
+    sudo yum install -y \
+    openssl-devel \
+    libX11-devel libXext-devel libXinerama-devel libXcursor-devel libxkbfile-devel \
+    libXv-devel libXtst-devel libXi-devel libXdamage-devel libXrandr-devel \
+    alsa-lib-devel cups-devel ffmpeg-devel
+    ;;
+esac
 
 #
 # Install FreeRDS dependencies
 #
-sudo apt-get install -y \
-libpciaccess-dev libpam0g-dev libpng12-dev libjpeg-dev intltool \
-libexpat1-dev libxml-libxml-perl libtool bison flex xsltproc \
-libfreetype6-dev libfontconfig1-dev libpixman-1-dev xutils-dev \
-x11proto-gl-dev mesa-common-dev libgl1-mesa-dev xorg-dev \
-protobuf-c-compiler libprotobuf-c0 libprotobuf-dev libprotobuf-c0-dev \
-libboost-dev qt4-dev-tools
+case $LINUX_DISTRO_NAME in
+  Ubuntu)
+    sudo apt-get install -y \
+    libpciaccess-dev libpam0g-dev libpng12-dev libjpeg-dev intltool \
+    libexpat1-dev libxml-libxml-perl libtool bison flex xsltproc \
+    libfreetype6-dev libfontconfig1-dev libpixman-1-dev xutils-dev \
+    x11proto-gl-dev mesa-common-dev libgl1-mesa-dev xorg-dev \
+    protobuf-c-compiler libprotobuf-c0 libprotobuf-dev libprotobuf-c0-dev \
+    libboost-dev qt4-dev-tools
+    ;;
+
+  CentOS)
+    sudo yum install -y \
+    finger patch gcc gcc-c++ make autoconf libtool automake pkgconfig \
+    libpciaccess-devel openssl-devel gettext file pam-devel libjpeg-devel pixman-devel \
+    libX11-devel libXfixes-devel libXfont-devel xorg-x11-proto-devel xorg-x11-xtrans-devel \
+    flex bison libxslt perl-libxml-perl xorg-x11-font-utils xmlto-tex docbook-utils-pdf \
+    boost-devel qt4-devel
+    ;;
+esac
 
 #
-# Install the correct version of Thrift
+# Install the correct version of Apache Thrift
 #
-THRIFT_VERSION=0.9.1
+THRIFT_VERSION=0.9.0
 THRIFT_FOLDER=thrift-$THRIFT_VERSION
 THRIFT_ARCHIVE=$THRIFT_FOLDER.tar.gz
 THRIFT_URL=http://archive.apache.org/dist/thrift/$THRIFT_VERSION/$THRIFT_ARCHIVE
 
 RESULT=`thrift --version`
-if [ "$RESULT" != "Thrift version $THRIFT_VERSION" ]; then
+if [ "$RESULT" == "" ]; then
   # Download compressed archive
   pushd ~/Downloads
   wget $THRIFT_URL
@@ -132,6 +191,63 @@ if [ "$RESULT" != "Thrift version $THRIFT_VERSION" ]; then
   popd
   rm $THRIFT_ARCHIVE
   popd
+else
+  echo Found $RESULT
+fi
+
+#
+# Install the correct version of Google protocol buffers
+#
+PROTOBUF_VERSION=2.4.1
+PROTOBUF_FOLDER=protobuf-$PROTOBUF_VERSION
+PROTOBUF_ARCHIVE=$PROTOBUF_FOLDER.tar.gz
+PROTOBUF_URL=https://protobuf.googlecode.com/files/$PROTOBUF_ARCHIVE
+
+RESULT=`protoc --version`
+if [ "$RESULT" == "" ]; then
+  # Download compressed archive
+  pushd ~/Downloads
+  wget $PROTOBUF_URL
+  popd
+
+  # Unpack compressed archive
+  pushd ~/Downloads
+  tar xvf $PROTOBUF_ARCHIVE
+  pushd $PROTOBUF_FOLDER
+  ./configure --prefix=/usr
+  make
+  sudo make install
+  popd
+  rm $PROTOBUF_ARCHIVE
+  popd
+else
+  echo Found $RESULT
+fi
+
+PROTOBUFC_VERSION=0.15
+PROTOBUFC_FOLDER=protobuf-c-$PROTOBUFC_VERSION
+PROTOBUFC_ARCHIVE=$PROTOBUFC_FOLDER.tar.gz
+PROTOBUFC_URL=https://protobuf-c.googlecode.com/files/$PROTOBUFC_ARCHIVE
+
+RESULT=`protoc-c --version`
+if [ "$RESULT" == "" ]; then
+  # Download compressed archive
+  pushd ~/Downloads
+  wget $PROTOBUFC_URL
+  popd
+
+  # Unpack compressed archive
+  pushd ~/Downloads
+  tar xvf $PROTOBUFC_ARCHIVE
+  pushd $PROTOBUFC_FOLDER
+  ./configure --prefix=/usr
+  make
+  sudo make install
+  popd
+  rm $PROTOBUFC_ARCHIVE
+  popd
+else
+  echo Found $RESULT
 fi
 
 #
