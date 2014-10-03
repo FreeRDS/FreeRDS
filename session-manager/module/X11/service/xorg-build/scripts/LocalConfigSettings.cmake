2df06bb47e0dff15e8a10c1e469c9fe42bf7544a
Marc-André Moreau <marcandre.moreau@gmail.com>
2014-10-02 22:40:24 -0400

    freerds: move modules around, move get rid of common directory

--- session-manager/module/X11/service/xorg-build/scripts/LocalConfigSettings.cmake
+++
@@ -1,5 +0,0 @@
-set(XOBJBASE_RELATIVE "../xorg-server/build-main")
-# ubuntu <= 13.10, debian wheezy
-set(ADDITIONAL_REQUIRED_LIBS "-lselinux -laudit -lgcrypt")
-# ubuntu >= 13.10
-#set(ADDITIONAL_REQUIRED_LIBS "-lselinux -laudit -lgcrypt -lglapi")
